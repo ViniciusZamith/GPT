ae971d778d2deb8ce7fd2b4bc9a983770364ae9c
barcante <diego.pires@uniriotec.br>
2018-05-25 18:45:47 -0300

    Alteredo dialect hibernate para conexão com banco.

--- src/main/java/br/com/gpt/DataConfiguration.java
+++ src/main/java/br/com/gpt/DataConfiguration.java
@@ -1,34 +1,34 @@
-//package br.com.gpt;
-//
-//import javax.sql.DataSource;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.jdbc.datasource.DriverManagerDataSource;
-//import org.springframework.orm.jpa.JpaVendorAdapter;
-//import org.springframework.orm.jpa.vendor.Database;
-//import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-//
-//@Configuration
-//public class DataConfiguration {
-//
-//	@Bean
-//	public DataSource dataSource() {
-//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-//		dataSource.setUrl("jdbc:mysql://sql10.freemysqlhosting.net:3306");
-//		dataSource.setUsername("root");
-//		dataSource.setPassword("UFlEEYuckr");
-//		return dataSource;
-//	}
-//
-//	@Bean
-//	public JpaVendorAdapter JpaVendorAdapter() {
-//		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-//		adapter.setDatabase(Database.MYSQL);
-//		adapter.setShowSql(true);
-//		adapter.setGenerateDdl(true);
-//		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
-//		return adapter;
-//	}
-//}
+package br.com.gpt;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DataConfiguration {
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/gpt");
+		dataSource.setUsername("gptMaster");
+		dataSource.setPassword("gpt123");
+		return dataSource;
+	}
+
+	@Bean
+	public JpaVendorAdapter JpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+		return adapter;
+	}
+}
